8e0a2801ae38ebeef1b83e010de90515640c83e0
Tam <21130168@st.hcmuaf.edu.vn>
2024-05-24 20:20:30 +0700

    add delCart, increaseCart, decreasement in Action.js

--- src/store/Action.js
+++ src/store/Action.js
@@ -7,12 +7,26 @@ export const loadProduct=(data)=>{
 export const addCart=(product)=>{
     return {
         type:'cart/add',
-        payload:product
+        payload: product
     }
 }
-export const delCart=(product)=>{
+
+export const delCart=(productid)=>{
     return {
         type:'cart/del',
-        payload:product
+        payload:productid
+    }
+}
+export const increaseCart=(productid)=>{
+    return {
+        type:'cart/increasement',
+        payload:productid
+    }
+}
+export const decreasement = (productid) =>{
+    return{
+
+        type:'cart/decreasement',
+        payload: productid
     }
 }
