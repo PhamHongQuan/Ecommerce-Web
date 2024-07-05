4cde428ca0e4a0055a7d544084aa6eb4ab330fe2
Tam <21130168@st.hcmuaf.edu.vn>
2024-07-06 02:35:48 +0700

    fix when no login and don't choice size and color

--- src/component/Navigation/Pagination.js
+++ src/component/Navigation/Pagination.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { products } from '../../data/ProductData';
-import {NavLink} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {useSelector } from 'react-redux';
-import {addCart} from "../../store/Action"; // Đường dẫn đến tệp ProductData
+import {addCart} from "../../store/Action";
+import {MDBContainer} from "mdb-react-ui-kit";
+import Navbar from "./navbar";
+import Footers from "../Footer/Footers"; // Đường dẫn đến tệp ProductData
 
 const Products = ({product}) => {
     const [productList, setProductList] = useState([]);
@@ -11,10 +14,22 @@ const Products = ({product}) => {
     const [productsPerPage] = useState(20); // Số sản phẩm mỗi trang
     const dispatch = useDispatch();
 
+    const [selectedProduct, setSelectedProduct] = useState(null);
     // Tính toán số lượng trang dựa trên số lượng sản phẩm và số sản phẩm mỗi trang
     const totalPages = Math.ceil(products.length / productsPerPage);
-    const currentUser = useSelector((state) => state.user?.currentUser); // Sử dụng optional chaining
+    const cart = useSelector(state => state.cart);
+    const currentUser = useSelector(state => state.currentUser);
+    const [buttonColors, setButtonColors] = useState({});
 
+    if(currentUser != null){
+        const userCart = cart.find(item => item.username === currentUser.username);
+        const productsOfCart = userCart.products;
+    }
+    useEffect(() => {
+        // Lấy màu sắc từ localStorage khi component được render
+        const storedColors = JSON.parse(localStorage.getItem('buttonColors')) || {};
+        setButtonColors(storedColors);
+    }, []);
     useEffect(() => {
         // Cắt lấy danh sách sản phẩm cho trang hiện tại
         const startIndex = (currentPage - 1) * productsPerPage;
@@ -28,17 +43,23 @@ const Products = ({product}) => {
     };
 
     //thay doi mau btn
-    const [buttonColors, setButtonColors] = useState({});
-    useEffect(() => {
-        // Lấy màu sắc từ localStorage khi component được render
-        const storedColors = JSON.parse(localStorage.getItem('buttonColors')) || {};
-        setButtonColors(storedColors);
-    }, []);
+
 
     const [isAddedToCart, setIsAddedToCart] = useState(false);
-    const handleAddToCart =({id, name, img, des,price}) =>{
-        dispatch(addCart({id, name, img, des, price}));
+    const handleAddToCart =({id, name, img, des,price},selectedSize,selectedColor,selectedQuantity) =>{
+
+        if (selectedColor !== null && selectedSize !== null) {
+            dispatch(addCart({  id,  name,  img,  des,  price,size: selectedSize, color: selectedColor,quantity: selectedQuantity
+            }));
+            alert("Đã thêm sản phẩm vào giỏ hàng");
+
+
+        } else {
+            alert("Vui lòng chọn đầy đủ kích thước, màu sắc và số lượng trước khi thêm vào giỏ hàng");
+        }
+
     };
+
     const handleRemoveFromCart = (productId) => {
         // Logic để xóa sản phẩm khỏi giỏ hàng
         console.log('Xóa sản phẩm khỏi giỏ hàng', productId);
@@ -73,9 +94,65 @@ const Products = ({product}) => {
         setInCart(!inCart); // Đảo ngược trạng thái giỏ hàng khi nhấn vào button
     };
 
+
+    const [showPopup, setShowPopup] = useState(false);
+    const handleClosePopup = () => {
+        setShowPopup(false);
+
+    };
+    const handlePopUp = (product) =>{
+        setShowPopup(true);
+        setSelectedProduct(product);
+        setSelectedSize(null);
+        setSelectedColor(null);
+        setQuantity(1);
+    }
+    const [selectedColor, setSelectedColor] = useState(
+        localStorage.getItem("selectedColor") || null
+    );
+    useEffect(() => {
+        localStorage.setItem("selectedColor", selectedColor);
+    }, [selectedColor]);
+
+    const [selectedSize, setSelectedSize] = useState(
+        localStorage.getItem("selectedSize") || null
+    );
+    useEffect(() => {
+        localStorage.setItem("selectedSize", selectedSize);
+    }, [selectedSize]);
+    const handleColorClick = (tint) => {
+        setSelectedColor(tint);
+    };
+    const handleColorClickSize = (size) => {
+        setSelectedSize(size);
+    };
+
+    useEffect(() => {
+        localStorage.setItem("selectedSize", selectedSize);
+    }, [selectedSize]);
+    const [selectedQuantity, setQuantity] = useState(() => {
+        const savedQuantity = localStorage.getItem('selectedQuantity');
+        return savedQuantity ? parseInt(savedQuantity, 10) : 1;
+    });
+    useEffect(() => {
+        localStorage.setItem('selectedQuantity', selectedQuantity);
+    }, [selectedQuantity]);
+
+    const handleIncrement = () => {
+        setQuantity((prevQuantity) => Math.min(prevQuantity + 1, 9999));
+    };
+
+    const handleDecrement = () => {
+        setQuantity((prevQuantity) => Math.max(prevQuantity - 1, 1));
+    };
+
+    const handleChange = (event) => {
+        const value = Math.max(1, Math.min(parseInt(event.target.value, 10) || 1, 9999));
+        setQuantity(value);
+    };
+
     return (
         <div>
-
             <div className="row">
                 {productList.map((product) => {
                     const formattedPrice = new Intl.NumberFormat('vi-VN', {
@@ -115,16 +192,87 @@ const Products = ({product}) => {
                                                 className={`btn ${buttonColor} d-flex align-items-center mb-2 mb-md-0`}
                                                 onClick={(e) => {
                                                     e.stopPropagation();
-                                                    handleAddToCart(product);
-                                                    changeButtonColor(product.id); // Đổi màu khi nhấp chuột
+                                                    handlePopUp(product);
+                                                  //  changeButtonColor(product.id); // Đổi màu khi nhấp chuột
                                                     // handleToggleCart(product.id);
                                                     // handleRemoveFromCart(product.id);
                                                 }}>
                                                 <i className="fa fa-cart-plus me-2" aria-hidden="true"></i>
                                                 {inCart ? 'Xóa' : 'Thêm'}
                                             </button>
+                                            {showPopup && selectedProduct &&(
+                                                <div id="popup">
+                                                    <div className="popup-content">
+                                                        <div className="d-flex">
+                                                            <p className="fw-bold mt-2">Chọn Size</p>
+                                                            <div className="ms-3" role="group" aria-label="First group">
+                                                                {Array.isArray(selectedProduct.size) && selectedProduct.size.map((size, index) => (
+                                                                    <button
+                                                                        key={index}
+                                                                        type="button"
+                                                                        className={`btn ${selectedSize === size ? "btn-primary" : "btn-outline-black"} me-2`}
+                                                                        onClick={() => handleColorClickSize(size)}
+                                                                    >
+                                                                        {size}
+                                                                    </button>
 
+                                                                ))}
 
+                                                            </div>
+                                                        </div>
+                                                        <div className="d-flex">
+                                                            <p className="fw-bold mt-2">Màu Sắc</p>
+                                                            <div className="ms-4" role="group" aria-label="">
+                                                                {Array.isArray(selectedProduct.tint) && selectedProduct.tint.map((tint, index) => (
+                                                                    <button
+                                                                        key={index}
+                                                                        type="button"
+                                                                        className={`btn ${selectedColor === tint ? "btn-primary" : "btn-outline-black"} me-2`}
+                                                                        onClick={() => handleColorClick(tint)}
+                                                                    >
+                                                                        {tint}
+                                                                    </button>
+                                                                ))}
+                                                            </div>
+                                                        </div>
+                                                        <div className="d-flex">
+                                                            <div className="quantity buttons_added">
+                                                                <input type="button" value="-" className="minus button is-form"
+                                                                       onClick={handleDecrement}/>
+                                                                <input type="number" id="quantity" className="input-text qty text" step="1"
+                                                                       min="1" max="9999" name="quantity" value={selectedQuantity} title="SL" size="4"
+                                                                       inputMode="numeric" onChange={handleChange}  style={{ textAlign: 'center' }}/>
+                                                                <input type="button" value="+" className="plus button is-form"
+                                                                       onClick={handleIncrement}/>
+                                                            </div>
+
+                                                        </div>
+                                                        <div className="card-footer">
+                                                            <button className="custom-button-pl" onClick={(e) => {
+                                                                e.stopPropagation();
+                                                                if(currentUser != null){
+                                                                    if(selectedSize != null && selectedColor != null  ){
+                                                                        handleAddToCart(selectedProduct,selectedSize,selectedColor,selectedQuantity);
+                                                                    }else {
+                                                                        alert("Bạn hãy chọn đầy đủ kích thước và màu sắc");
+                                                                    }
+                                                                }else {
+                                                                    alert("Bạn phải đăng nhạp");
+
+                                                                }
+
+                                                            }}>Thêm
+                                                            </button>
+                                                            <button className="custom-button-pl" onClick={(e) => {
+                                                                e.stopPropagation();
+                                                                handleClosePopup();
+                                                            }}>Đóng
+                                                            </button>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )}
                                         </div>
                                     </div>
                                 </div>
